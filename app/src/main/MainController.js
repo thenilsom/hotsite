2dab402db733d6ce2561c43c0ebb2aa9c3a5733c
Denilson Godinho de Oliveira <denilson_godinho@hotmail.com>
2017-11-13 17:04:57 -0200

    botão go to top

--- app/src/main/MainController.js
+++ app/src/main/MainController.js
@@ -1,5 +1,18 @@
   angular
        .module('app')
        .controller('MainController', ['$scope', function($scope){
-       
-       }]);
+
+          //controla o hide/show do botão ir para topo
+	      $(window).scroll(function () {
+	         if ($(this).scrollTop() > 100) {
+	         	$('.btn-go-top').addClass('display').fadeIn("slow");
+	         } else {
+	         	$('.btn-go-top').removeClass('display').fadeOut();
+	         }
+	      });
+       		
+       		/*Função voltar para o topo da pagina*/
+       		$scope.gotoTop = function(){
+       			$("html, body").animate({ scrollTop: 0 }, 300);
+       		}
+    }]);
