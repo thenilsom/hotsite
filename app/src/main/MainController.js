6cd200a840ab7dbc3d139275c8a6a6a04a070803
Denilson Godinho de Oliveira <denilson_godinho@hotmail.com>
2017-11-23 14:54:46 -0200

    correção dos icones de ancora

--- app/src/main/MainController.js
+++ app/src/main/MainController.js
@@ -1,23 +1,30 @@
-  angular
+ angular
        .module('app')
-       .controller('MainController', ['$scope', function($scope){
+       .controller('MainController', ['$scope', '$location', '$anchorScroll',
+        function($scope, $location, $anchorScroll){
 
         
           //controla o hide/show do botão ir para topo quando chegar no fim da pagina
-	      $(window).scroll(function () {
+        $(window).scroll(function () {
           //usar esse if para exibir o btn ir para o top quando o usuario rolar a pagina
           //if ($(this).scrollTop() > 100); 
 
           var maxTop = document.body.scrollHeight - (document.body.clientHeight + 300);
-	         if (parseInt($(this).scrollTop()) >= maxTop) {
-	         	$('.btn-go-top').addClass('display').fadeIn();
-	         } else {
-	         	$('.btn-go-top').removeClass('display').fadeOut();
-	         }
-	      });
-       		
-       		/*Função voltar para o topo da pagina*/
-       		$scope.gotoTop = function(){
-       			$("html, body").animate({ scrollTop: 0 }, 300);
-       		}
+           if (parseInt($(this).scrollTop()) >= maxTop) {
+            $('.btn-go-top').addClass('display').fadeIn();
+           } else {
+            $('.btn-go-top').removeClass('display').fadeOut();
+           }
+        });
+          
+          /*Função voltar para o topo da pagina*/
+          $scope.gotoTop = function(){
+            $("html, body").animate({ scrollTop: 0 }, 300);
+          }
+
+          /*rola a pagina até o id passado*/
+          $scope.goTo = function(id){
+            $location.hash(id);
+            $anchorScroll();
+          }
     }]);
