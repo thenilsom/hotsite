c7a297fd44b160bf90b34837818dc8c0464bedd1
Denilson Godinho de Oliveira <denilson_godinho@hotmail.com>
2017-12-12 17:37:34 -0200

    .

--- app/src/app.js
+++ app/src/app.js
@@ -2,7 +2,7 @@
   'use strict';
 
   // Prepare the 'users' module for subsequent registration of controllers and delegates
-  angular.module('app', [ 'ngMaterial', 'diretiva', 'angularFileUpload' ])
+  angular.module('app', [ 'ngMaterial' ])
   .run(function($mdDateLocale, $filter) {
         $mdDateLocale.formatDate = function(date) {
             return $filter('date')(date, "dd/MM/yyyy");
--- app/src/main/MainController.js
+++ app/src/main/MainController.js
@@ -1,7 +1,7 @@
  angular
        .module('app')
-       .controller('MainController', ['$scope', 'serviceUtil', 'validaService', 'FileUploader', 
-        function($scope, service, validador, FileUploader){
+       .controller('MainController', ['$scope', 'serviceUtil', 
+        function($scope, service){
 
                
           //controla o hide/show do botão ir para topo quando chegar no fim da pagina
@@ -27,78 +27,4 @@
            service.moveTo(id);
           }
 
-          /*************************FUNÇÕES DO FORMULÁRIO**********************/
-
-          $scope.passo = '1';
-          $scope.uploader = new FileUploader({
-            url : 'upload.php'
-          });
-          $scope.cadastro = {};
-
-
-          $scope.proximoPasso = function(){    
-            $scope.passo = service.obterProximoPasso($scope.passo);
-          }
-
-          $scope.passoAnterior = function(){
-           $scope.passo = service.obterPassoAnterior($scope.passo);
-          }
-
-          $scope.getLabelEtapa = function(etapa){
-            return service.labelEtapa(etapa);
-          }
-
-          /*Valida os dados do Pretendente*/
-          $scope.validarDadosPretendente = function(form){
-             $scope.errors = [];
-             validador.validarCamposObrigatorios(form, $scope.errors);
-              if(!validador.validarCpf($scope.cadastro.pretendente.cpf)){
-                $scope.errors.push("CPF inválido");
-              }
-
-              if(!validador.validarCpf($scope.cadastro.pretendente.cpfConjuge)){
-                $scope.errors.push("CPF Conjuge inválido");
-              }
-
-              if(!validador.validarEmail($scope.cadastro.pretendente.email)){
-                $scope.errors.push("EMAIL inválido");
-              }
-
-              if($scope.errors.length == 0){
-                $scope.proximoPasso();
-              }else{
-                setTimeout(function(){
-                  $scope.goTo('listErrors');
-                });
-                
-              }
-          }
-
-
-          $scope.getTotal = function(obj){
-            if(service.isNullOrEmpty(obj)) return 0;
-            return service.valorOuZeroSeNull(obj.aluguel) +
-                   service.valorOuZeroSeNull(obj.iptu) +
-                   service.valorOuZeroSeNull(obj.condominio) +
-                   service.valorOuZeroSeNull(obj.agua) +
-                   service.valorOuZeroSeNull(obj.luz) +
-                   service.valorOuZeroSeNull(obj.gas);
-          }
-
-          $scope.pesquisarCep = function(obj){
-            //se o cep for valido efetua a consulta no webservice
-            var cep = obj.cep.replace(/\.|\-/g, '');
-            if(/^[0-9]{8}$/.test(cep)){
-              service.consultarCep(cep, function(dados){
-                if(dados !== null){
-                  obj.estado = dados.uf;
-                  obj.cidade = dados.localidade.toUpperCase();
-                  obj.endereco = dados.logradouro.toUpperCase();
-                  obj.bairro = dados.bairro.toUpperCase();
-                  obj.complemento = dados.complemento.toUpperCase();
-                  $scope.$digest();
-                }
-              })
-            }
-          }
     }]);
